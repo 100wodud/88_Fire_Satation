1d8535f175adeee6d41e260114e736697298cd30
Jaeyoung Paik <100wodud@gmail.com>
2024-01-29 15:38:36 +0900

    [feature] 사운드 추가

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -27,6 +27,8 @@ public class GameManager : MonoBehaviour
     // global time
     public float time = 0;
     public bool timerSet = false;
+
+    //난이도별 최고시간
     public float EasyTime = 0;
     public float NormalTime = 0;
     public float HardTime = 0;
@@ -34,7 +36,6 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //SceneManager.LoadScene("StartScene");
 
     }
 
@@ -44,22 +45,24 @@ public class GameManager : MonoBehaviour
         if (timerSet)
         {
             time += Time.deltaTime;
-            Debug.Log(time);
         }
         
     }
 
+    // 게임 레벨 변경
     public void GameLevelChange(int game_level)
     {
         GameLevel = game_level;
     }
 
+    // 게임 시작시 타이머 리셋 및 시작
     public void StartTimer()
     {
         time = 0;
         timerSet = true;
     }
 
+    // 게임 타이머 종료 및 최고시간 저장
     public void StopTimer() 
     { 
         timerSet = false;
